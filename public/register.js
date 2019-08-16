95aa7a9f0e0fe7efde246f1eb0245d5fef70515a
Sean Philippi <sean.philippi@gmail.com>
2019-08-15 21:22:04 -0500

    register page

---
+++ public/register.js
@@ -0,0 +1,36 @@
+const newUser = {
+  username: null,
+  email: null,
+  password: null,
+  password2: null
+};
+
+function onTextChange(e) {
+  newUser[e.target.id] = e.target.value;
+};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  document.querySelector('.input').value = '';
+  passwordError();
+  incompleteError();
+};
+
+function passwordError() {
+  if (newUser.password !== newUser.password2) {
+    document.querySelector('#errors').innerText = 'your passwords must match!'
+    document.querySelector('#errors').className = 'd-block';
+  }
+};
+
+// check all inputs to make sure they are not empty
+function incompleteError() {
+  let numOfFields = 0;
+  Array(document.querySelector('input')).forEach((input) => {
+    if (input.value === '') {
+      numOfFields++;
+      console.log(numOfFields)
+    }
+    return numOfFields !== 0 ? document.querySelector('#errors').innerText = `${numOfFields} fields are incomplete.  Please fill out all fields.` : null;
+  })
+}
